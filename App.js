2ce6fa53ebfaecf17f959fc76d0c297f8ac40d54
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2023-09-23 17:38:34 +0700

    Added header styling

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home, { About } from "./components.js";
+import Home, { About, Contact } from "./components.js";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,13 +10,41 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="About" component={About} />
+        <Tab.Screen name="Home" component={Home} options={{
+          headerStyle: {
+            backgroundColor: 'green',
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: 'white',
+          },
+          headerTitleAlign: 'center',
+        }}/>
+        <Tab.Screen name="About" component={About} options={{
+          headerStyle: {
+            backgroundColor: 'green',
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: 'white',
+          },
+          headerTitleAlign: 'center',
+        }}/>
+        <Tab.Screen name="Contact" component={Contact} options={{
+          headerStyle: {
+            backgroundColor: 'green',
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: 'white',
+          },
+          headerTitleAlign: 'center',
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-
+  
 });
