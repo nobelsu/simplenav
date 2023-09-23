fc348d8b48011c2d842cdf34492fb4b94d72cb25
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2023-09-23 18:35:43 +0700

    Added styling and icons to tabs

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home, { About, Contact } from "./components.js";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -19,6 +20,10 @@ export default function App() {
             color: 'white',
           },
           headerTitleAlign: 'center',
+          tabBarLabelPosition: 'below-icon',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={"#90EE90"} size={size} />
+          ),
         }}/>
         <Tab.Screen name="About" component={About} options={{
           headerStyle: {
@@ -29,6 +34,10 @@ export default function App() {
             color: 'white',
           },
           headerTitleAlign: 'center',
+          tabBarLabelPosition: 'below-icon',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="information" color={"#90EE90"} size={size} />
+          ),
         }}/>
         <Tab.Screen name="Contact" component={Contact} options={{
           headerStyle: {
@@ -39,6 +48,10 @@ export default function App() {
             color: 'white',
           },
           headerTitleAlign: 'center',
+          tabBarLabelPosition: 'below-icon',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="phone" color={"#90EE90"} size={size} />
+          ),
         }}/>
       </Tab.Navigator>
     </NavigationContainer>
