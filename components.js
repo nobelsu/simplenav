c30eba93c091a40705f4ece40016d79b950ba66f
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2023-09-23 16:49:34 +0700

    Added components and imported libraries

---
+++ components.js
@@ -0,0 +1,18 @@
+import { Text, View } from 'react-native';
+
+function Home() {
+    return (
+        <View>
+
+
+        </View>
+    );
+}
+
+function About() {
+    return (
+        <View>
+
+        </View>
+    );
+}
