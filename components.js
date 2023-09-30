a9e2d5b64f4b6d7e3bded040361958eff49b7c7e
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2023-09-30 09:03:22 +0700

    Added about page

--- components.js
+++ components.js
@@ -96,7 +96,47 @@ export function About() {
         backgroundColor: "white",
         flex: 1,
       }}
-    ></ScrollView>
+    >
+      <View style={styles.sep}>
+        <View style={styles.leftt}>
+          <Text style={styles.paragraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </View>
+        <View style={styles.rightt}>
+          <Image style={styles.logo2} source={require("./assets/icon.png")} />
+        </View>
+      </View>
+      <View style={styles.sep}>
+        <View style={styles.rightt}>
+          <Image style={styles.logo2} source={require("./assets/icon.png")} />
+        </View>
+        <View style={styles.leftt}>
+          <Text style={styles.paragraph}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -144,5 +184,22 @@ const styles = StyleSheet.create({
   dropdown: {
     rowTitleColor: "green",
     transitionDuration: "0.4s",
+  },
+  sep: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  leftt: {
+    flex: 1.2,
+    justifyContent: "center",
+  },
+  rightt: {
+    flex: 0.7,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo2: {
+    height: 150,
+    width: 150,
   },
 });
